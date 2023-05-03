c2fd9961c4b6bd8726e4cbd8ed3d75b922a140ce
Velhes <malo.behr@mewo-campus.fr>
2023-05-03 11:12:18 +0200

    oui

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
 provider "azurerm" {
   features {}
 }
+
+# State Backend
+
+terraform {
+  backend "azurerm" {
+  resource_group_name = "tfstate"
+  storage_account_name = "tfstate29638"
+  container_name = "tfstate"
+  key = "test.terraform.tfstate"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "prefix" {
   # A personnaliser avec votre nom
-  default = "smontri"
+  default = "malob"
 }
 
 variable "environment" {
@@ -12,5 +12,6 @@ variable "project" {
 }
 
 variable "ssh_key" {
-  default = ""
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDasUFgkc3I3cSMjyCc0eUsr4pUrOls75ho5nWxJj8eVfp0tjfN4Db+u+t9vaqRVkl3tpWyeloR4XyXBLkUpfAL3bAacjLAQHENpGgjgAZc1HnakCbKtqcdKbikq1Ae1BIF0oi5axdcJe0du3C8+aHIbcGUQ5csEtH9d52ebiVUpF8rduFpClQ4CMXwTVQ7Td6fG2rzfTFLST/C6X+Q6Wlzrk8zyRSvL1p8OAfb4nIq3UG4idyzBLvktShCg3TakJ1weIhkobtF9Ty7unbW1Ir+G+oDgZpSL7jF4ueTkVhqzaRYl15tJQhWanMIbH9NGpiNqisayM9/UxQlt9zGLA3MwPL02JvGO+5VR+s9/5LZSuQmf0PBq5qdEDAOqIZNihHcmiuxwLoNBTPV8vv6dC/NlALPuCy7Rasp48rdnmxl23XFta58j/UXSDmTxXXlgO9/Zraoy3q/Fvp397n0+B5C0Tpzz8j+jQBhv8qIKOWzfCszL877BwGnC360K6ef2QE= malo.behr@mewo-campus.fr
+"
 }
